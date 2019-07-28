4251b46fe005f01db67edf569fcebe7a6a0cd874
Nataly09 <natali.strashko@gmail.com>
2019-07-28 16:30:19 +0300

    homework

--- lesson_20/actions.js
+++
@@ -1,7 +0,0 @@
-export const REMOVE = 'REMOVE';
-export const ADD = 'ADD';
-export const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
-export const CHECK = 'CHECK';
-export const COUNTER_DEC = 'COUNTER_DEC';
-export const SET = 'SET';
-export const GET = 'GET';
--- lesson_20/middlewares/logger.js
+++
@@ -1,7 +0,0 @@
-
-
-export default (store) => (next) => (payload) => {
-    // console.log(payload.type, payload);
-    next(payload);
-    // console.log(store.getState().counter.count)
-}
